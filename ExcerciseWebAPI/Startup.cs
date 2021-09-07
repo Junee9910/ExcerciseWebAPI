db0a9c91571b1a68733fd8fd39ec2e987509f74d
JuneeDinh <dinhttnhung9910@gmail.com>
2021-09-07 15:15:31 +0700

    change Cors

--- ExcerciseWebAPI/Startup.cs
+++ ExcerciseWebAPI/Startup.cs
@@ -46,17 +46,12 @@ namespace ExcerciseWebAPI
             services.AddScoped<IInstructorService, InstructorService>();
             services.AddScoped<ICourseService, CourseService>();
 
-            services.AddCors();
+            services.AddCors(x => x.AddDefaultPolicy(p => p.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors(options =>
-            options.WithOrigins("http://localhost:4200")
-            .AllowAnyMethod()
-            .AllowAnyHeader());
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -65,6 +60,8 @@ namespace ExcerciseWebAPI
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors();
 
             app.UseRouting();
 
